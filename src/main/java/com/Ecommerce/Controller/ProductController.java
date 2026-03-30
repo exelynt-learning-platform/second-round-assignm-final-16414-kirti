9be85f2e3688bf5fe12ae951867637877fa98f1c
Kirti Navnath Dhekale <155144900+KirtiDhekale@users.noreply.github.com>
2026-03-30 16:51:20 +0530

    Update ProductController.java

--- src/main/java/com/Ecommerce/Controller/ProductController.java
+++ src/main/java/com/Ecommerce/Controller/ProductController.java
@@ -2,17 +2,9 @@ package com.Ecommerce.Controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.Ecommerce.Model.Product;
-
 import com.Ecommerce.Service.ProductService;
 
 @RestController
@@ -51,4 +43,3 @@ public class ProductController {
         return "Deleted successfully";
     }
 }
-
