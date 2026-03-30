078b2ed78f78613055cdf98b182cc0a9beed6c48
Kirti Navnath Dhekale <155144900+KirtiDhekale@users.noreply.github.com>
2026-03-30 14:59:16 +0530

    Update SecurityConfig.java

--- src/main/java/com/Ecommerce/Security/SecurityConfig.java
+++ src/main/java/com/Ecommerce/Security/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.Ecommerce.Security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,30 +14,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private JwtFilter jwtFilter;   
 
-	    @Bean
-	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	      		http
-    .csrf().disable()
-    .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/auth/**").permitAll()   
-        .anyRequest().authenticated()              
-    );
+        http
+            .csrf().disable()
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()   
+                .requestMatchers("/admin/**").hasRole("ADMIN") 
+                .requestMatchers("/user/**").hasRole("USER")   
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); 
 
-	
-    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
-	        return http.build();
-	    }
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
-	
-	
-	
-	
