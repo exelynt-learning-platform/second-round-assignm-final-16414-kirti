8143605784b9e042726019d770de8c4cbd5a547b
Kirti Navnath Dhekale <155144900+KirtiDhekale@users.noreply.github.com>
2026-03-30 16:06:14 +0530

    Update CartService.java

--- src/main/java/com/Ecommerce/Service/CartService.java
+++ src/main/java/com/Ecommerce/Service/CartService.java
@@ -1,8 +1,6 @@
 
 package com.Ecommerce.Service;
 
-import java.util.List;
-package com.Ecommerce.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
