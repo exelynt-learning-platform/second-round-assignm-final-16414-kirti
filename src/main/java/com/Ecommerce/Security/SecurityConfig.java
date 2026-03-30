feaca53e891c88ab49de3fd8b3bf32ca0f8bca3a
Kirti Navnath Dhekale <155144900+KirtiDhekale@users.noreply.github.com>
2026-03-30 14:57:29 +0530

    Update SecurityConfig.java

--- src/main/java/com/Ecommerce/Security/SecurityConfig.java
+++ src/main/java/com/Ecommerce/Security/SecurityConfig.java
@@ -18,11 +18,15 @@ public class SecurityConfig {
 	    @Bean
 	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	        http
-	            .csrf(csrf -> csrf.disable())
-	            .authorizeHttpRequests(auth -> auth
-	                .anyRequest().permitAll()
-	            );
+	      		http
+    .csrf().disable()
+    .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/auth/**").permitAll()   
+        .anyRequest().authenticated()              
+    );
+
+	
+    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 	        return http.build();
 	    }
