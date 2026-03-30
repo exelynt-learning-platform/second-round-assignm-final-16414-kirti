1e47b686968cc8dd768576b522a3035ddc6e06fe
Kirti Navnath Dhekale <155144900+KirtiDhekale@users.noreply.github.com>
2026-03-30 16:03:27 +0530

    Update CartService.java

--- src/main/java/com/Ecommerce/Service/CartService.java
+++ src/main/java/com/Ecommerce/Service/CartService.java
@@ -1,7 +1,10 @@
+
 package com.Ecommerce.Service;
 
 import java.util.List;
+package com.Ecommerce.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Ecommerce.Model.Cart;
@@ -20,24 +23,28 @@ public class CartService {
     @Autowired
     private ProductRepository productRepo;
 
+    @Autowired
+    private UserRepository userRepo; 
+
     public Cart addToCart(Long userId, Long productId, int quantity) {
 
         
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
         Product product = productRepo.findById(productId)
                 .orElseThrow(() -> new RuntimeException("Product not found"));
 
-
-        Cart existingItem = cartRepo.findByUserIdAndProductId(userId, productId);
+        Cart existingItem = cartRepo.findByUser_IdAndProduct_Id(userId, productId);
 
         if (existingItem != null) {
-            
             existingItem.setQuantity(existingItem.getQuantity() + quantity);
             return cartRepo.save(existingItem);
         }
 
         
         Cart cart = new Cart();
-        cart.setUserId(userId);
+        cart.setUser(user);          
         cart.setProduct(product);
         cart.setQuantity(quantity);
 
