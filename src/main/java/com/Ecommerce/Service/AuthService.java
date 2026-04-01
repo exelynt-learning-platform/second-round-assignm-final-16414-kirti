7d7571e4fcf75dc63c5d42f6251a9eeb793f3b37
Kirti Navnath Dhekale <155144900+KirtiDhekale@users.noreply.github.com>
2026-04-01 22:27:52 +0530

    Update AuthService.java

--- src/main/java/com/Ecommerce/Service/AuthService.java
+++ src/main/java/com/Ecommerce/Service/AuthService.java
@@ -1,18 +1,46 @@
 package com.Ecommerce.Service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
+
+import com.Ecommerce.Model.User;
+import com.Ecommerce.Repository.UserRepository;
 
 @Service
-public class PaymentService {
+public class AuthService {
 
-    public String createPayment(Double amount) {
+    private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
 
-        if (amount == null || amount <= 0) {
-            throw new RuntimeException("Invalid payment amount");
+    public AuthService(UserRepository userRepo,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(User user) {
+
+        if (userRepo.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("User already exists");
         }
 
         
-        return "PAY_" + UUID.randomUUID().toString();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepo.save(user);
+    }
+
+    
+    public User login(String email, String password) {
+
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+    
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return user;
     }
 }
